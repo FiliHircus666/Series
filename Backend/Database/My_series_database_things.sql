851ff7a9a05dfca2afea1add2b57936b2ed8a9e1
FiliHircus666 <citromak17@gmail.com>
2022-03-28 13:05:41 +0200

    Adatbázis hozzárendelés

---
+++ Backend/Database/My_series_database_things.sql
@@ -0,0 +1,33 @@
+﻿SELECT * FROM user;
+SELECT * FROM series;
+SELECT * FROM favorite;
+SELECT * FROM comment;
+
+  INSERT INTO user 
+    (userName,password,`E-mail`,permission)
+    VALUES
+    ('Admin','AdminPassword','Admin@gmail.com',1),
+    ('User#234','UserPassword#234','User#234@gmail.com',2),
+    ('User#235','UserPassword#235','User#235@gmail.com',2);
+    
+
+  INSERT INTO series
+    (name,`Release`,ageLimit)
+    VALUES
+    ('The Last Kingdom','2015',16),
+    ('Vikings','2013',16),
+    ('Game of Thrones','2011',18);
+
+  INSERT INTO favorite
+    (userId,seriesId,Watched)
+    VALUES
+    (2,3,'watched'),
+    (3,1,'in progress');
+
+    INSERT INTO comment
+      (userId,comment,seriesId,ratePoint)
+      VALUES
+      (2,'I not like this',1,5),
+      (2,'Not bad',3,3);
+
+DELETE FROM comment WHERE id = 4;
